ee2041f63e50903ab1cad1819d5cce3714afddff
Atlas <nikkiraescalera@gmail.com>
2024-01-27 08:39:41 -0500

    e

--- DateClass.cs
+++ DateClass.cs
@@ -1,1 +1,18 @@
 // Insert Class Data Here
+
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, I'm attached to a GameObject!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Code to execute every frame
+    }
+}
