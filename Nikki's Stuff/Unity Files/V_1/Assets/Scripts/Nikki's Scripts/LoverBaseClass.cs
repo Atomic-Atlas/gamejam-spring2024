57e45f24967e12a9915aa0340693873735d56f07
Atlas <nikkiraescalera@gmail.com>
2024-01-27 11:50:15 -0500

    e

--- Nikki's Stuff/Unity Files/V_1/Assets/Scripts/Nikki's Scripts/LoverBaseClass.cs
+++ Nikki's Stuff/Unity Files/V_1/Assets/Scripts/Nikki's Scripts/LoverBaseClass.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class LoverBaseClass : MonoBehaviour
 {
 
-public class Book{
-        public string title;
-        public string author;
-        public static string staticAttribute = "My Static Attribute";
+    public class Book{
+            public string title;
+            public string author;
+            public static string staticAttribute = "My Static Attribute";
 
-        public void ReadBook(){
-        Console.WriteLine($"Reading {this.title} by {this.author}");
-     }
-}
+            public void ReadBook(){
+            Console.WriteLine($"Reading {this.title} by {this.author}");
+        }
+    }
 
-    Book book1 = new Book();
-    book1.title = "Harry Potter";
-    book1.author = "JK Rowling";
+        Book book1 = new Book();
+        book1.title = "Harry Potter";
+        book1.author = "JK Rowling";
 
-    book1.ReadBook();
-    Console.WriteLine(book1.title);
+        book1.ReadBook();
+        Console.WriteLine(book1.title);
 }
